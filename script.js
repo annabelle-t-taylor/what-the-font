0a49c8678d068fce2da2af921513e00ac9f79c05
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-02-05 16:11:15 -0500

    MVP for single question

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const questionNumEl = document.querySelector('.question-num')
 const questionString = document.querySelector('.string')
 const answersEl = document.querySelector('.answers')
-let score = document.querySelector('.score-num')
+let scoreEl = document.querySelector('.score-num')
+let score = 0
 const deckOfQuestions = []
 
 class Question{
@@ -17,25 +18,34 @@ class Question{
 
 
 const questionOne = new Question(1,'Times New Roman','Verdana','Ubuntu','Franklin B Gothic',3)
-//const questionTwo = new Question(2,"Arial","Comic Sans MS","Georgia","Garamond")
-
 deckOfQuestions.push(questionOne)
 
 for (let i = 0; i < deckOfQuestions.length; i++){
+    /*Question: is it better to send in the index to deckOfQuestions, or the object that lies at index i? */
     setQuestionFont(i)
     setAnswers(i)
 
     answersEl.addEventListener('click', function(evt){
-        if (evt.target.tagName === "A")
-            checkAnswer(i)
+        if (evt.target.tagName === "A"){
+           let input = evt.target.dataset.answerNum
+            checkAnswer(i,input)
+        }
         })
     }
-    //check answer
-    //update score
     //go onto next question and restart cycle
 
-function checkAnswer(i){
-    console.log(deckOfQuestions[i])
+function checkAnswer(i,userInput){
+    if (deckOfQuestions[i].answer == userInput){
+        console.log("Correct!")
+        updateScore()
+    }
+    else
+        console.log("Incorrect")
+}
+
+function updateScore(){
+    score+=10
+    scoreEl.innerText = score
 }
 function setQuestionFont(i){
     if (deckOfQuestions[i].answer === 1){
@@ -56,18 +66,22 @@ function setAnswers(i){
 
     const ans1 = document.createElement('a')
     ans1.innerText = deckOfQuestions[i].font1
+    ans1.dataset.answerNum = '1'
     answersEl.appendChild(ans1)
 
     const ans2 = document.createElement('a')
     ans2.innerText = deckOfQuestions[i].font2
+    ans2.dataset.answerNum = '2'
     answersEl.appendChild(ans2)
 
     const ans3 = document.createElement('a')
     ans3.innerText = deckOfQuestions[i].font3
+    ans3.dataset.answerNum = '3'
     answersEl.appendChild(ans3)
 
     const ans4 = document.createElement('a')
     ans4.innerText = deckOfQuestions[i].font4
+    ans4.dataset.answerNum = '4'
     answersEl.appendChild(ans4)
 }
 
