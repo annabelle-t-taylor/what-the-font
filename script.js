e50d4a7e09a670c83364103731ecdcedebd372ba
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-02-05 17:02:48 -0500

    Pass in variables instead of reference indices

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//https://stackoverflow.com/questions/5551378/javascript-pausing-execution-of-function-to-wait-for-user-input reference
+
 const questionNumEl = document.querySelector('.question-num')
-const questionString = document.querySelector('.string')
+const sampleString = document.querySelector('.string')
 const answersEl = document.querySelector('.answers')
 const resetEl = document.querySelector('.reset')
+const resultEl = document.querySelector('.result')
 let scoreEl = document.querySelector('.score-num')
 let score = 0
 const deckOfQuestions = []
@@ -22,27 +25,30 @@ const questionOne = new Question(1,'Times New Roman','Verdana','Ubuntu','Frankli
 deckOfQuestions.push(questionOne)
 //const questionTwo = new Question(2,'Courier New','Arial','Comic Sans','Roboto',1)
 //deckOfQuestions.push(questionOne,questionTwo)
+presentQuestion(deckOfQuestions[0])
 
-for (let i = 0; i < deckOfQuestions.length; i++){
-    /*Question: is it better to send in the index to deckOfQuestions, or the object that lies at index i? */
-    setQuestionFont(i)
-    setAnswers(i)
+function presentQuestion(deckQuestion){
+    setStringFont(deckQuestion)
+    setAnswers(deckQuestion)
     answersEl.addEventListener('click', function(evt){
         if (evt.target.tagName === "A"){
-            let input = evt.target
-            checkAnswer(i,input)
+            let domInput = evt.target
+            checkAnswer(deckQuestion,domInput)
         }
     })
 }
 
-function checkAnswer(i,userInput){
-    if (deckOfQuestions[i].answer == userInput.dataset.answerNum){
-        console.log("Yay!")
+function checkAnswer(question,userInput){
+    if (question.answer == userInput.dataset.answerNum){
+        userInput.style.backgroundColor = "lightgreen"
+        resultEl.innerText = "Correct! Onto the next question."
         updateScore()
-        userInput.style.backgroundColor = "green"
     }
     else{
-        userInput.style.backgroundColor = "red"
+        const correctAnswer = document.querySelector(`[data-answer-num='${question.answer}']`)
+        correctAnswer.style.backgroundColor = "lightgreen"
+        userInput.style.backgroundColor = "lightcoral"
+        resultEl.innerText = "Sorry, that's incorrect. Onto the next question."
     }
 }
 
@@ -51,40 +57,40 @@ function updateScore(){
     scoreEl.innerText = score
 }
 
-function setQuestionFont(i){
-    if (deckOfQuestions[i].answer === 1){
-        questionString.style.fontFamily = deckOfQuestions[i].font1
+function setStringFont(question){
+    if (question.answer === 1){
+        sampleString.style.fontFamily = question.font1
     }
-    else if (deckOfQuestions[i].answer === 2){
-        questionString.style.fontFamily = deckOfQuestions[i].font2
+    else if (question.answer === 2){
+        sampleString.style.fontFamily = question.font2
     }
-    else if (deckOfQuestions[i].answer === 3){
-        questionString.style.fontFamily = deckOfQuestions[i].font3
+    else if (question.answer === 3){
+        sampleString.style.fontFamily = question.font3
     }
-    else if (deckOfQuestions[i].answer === 4){
-        questionString.style.fontFamily = deckOfQuestions[i].font4
+    else if (question.answer === 4){
+        sampleString.style.fontFamily = question.font4
     }
 }
-function setAnswers(i){
-    questionNumEl.innerHTML = deckOfQuestions[i].qNum
+function setAnswers(question){
+    questionNumEl.innerHTML = question.qNum
 
     const ans1 = document.createElement('a')
-    ans1.innerText = deckOfQuestions[i].font1
+    ans1.innerText = question.font1
     ans1.dataset.answerNum = '1'
     answersEl.appendChild(ans1)
 
     const ans2 = document.createElement('a')
-    ans2.innerText = deckOfQuestions[i].font2
+    ans2.innerText = question.font2
     ans2.dataset.answerNum = '2'
     answersEl.appendChild(ans2)
 
     const ans3 = document.createElement('a')
-    ans3.innerText = deckOfQuestions[i].font3
+    ans3.innerText = question.font3
     ans3.dataset.answerNum = '3'
     answersEl.appendChild(ans3)
 
     const ans4 = document.createElement('a')
-    ans4.innerText = deckOfQuestions[i].font4
+    ans4.innerText = question.font4
     ans4.dataset.answerNum = '4'
     answersEl.appendChild(ans4)
 }
@@ -93,12 +99,26 @@ resetEl.addEventListener('click',function(evt){
     evt.preventDefault()
     score = 0
     scoreEl.innerText = score
+    resultEl.innerText = ""
 })
 
 /* 
 function sentenceGenerator(){
     //generate a line of Lorem Ipsum
     //assign it to a variable
-    //set questionString.innerText to the value of that variable
+    //set sampleString.innerText to the value of that variable
 }
 */
+
+//resumeGame function calls these other functions on the next element in deckOfQuestions
+// for (let i = 0; i < deckOfQuestions.length; i++){
+//     /*Question: is it better to send in the index to deckOfQuestions, or the object that lies at index i? */
+//     setQuestionFont(i)
+//     setAnswers(i)
+//     answersEl.addEventListener('click', function(evt){
+//         if (evt.target.tagName === "A"){
+//             let input = evt.target
+//             checkAnswer(i,input)
+//         }
+//     })
+// }
