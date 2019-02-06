74a6767cbeed5c2d04f00ebed4efff4ff50f759c
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-02-06 09:52:53 -0500

    Refactor setAnswers to only insert text instead of recreating elements; allows to move between questions

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const questionNumEl = document.querySelector('.question-num')
 const sampleString = document.querySelector('.string')
 const answersEl = document.querySelector('.answers')
 const resetEl = document.querySelector('.reset')
-const resultEl = document.querySelector('.result')
+const resultTextEl = document.querySelector('.result-text')
+const nextQuestion = document.querySelector('.next-question')
 let scoreEl = document.querySelector('.score-num')
+
+const ans1 = document.querySelector('[data-answer-num^="1"]')
+const ans2 = document.querySelector('[data-answer-num^="2"]')
+const ans3 = document.querySelector('[data-answer-num^="3"]')
+const ans4 = document.querySelector('[data-answer-num^="4"]')
+
 let score = 0
 const deckOfQuestions = []
 
@@ -22,9 +29,9 @@ class Question{
 
 
 const questionOne = new Question(1,'Times New Roman','Verdana','Ubuntu','Franklin B. Gothic',3)
-deckOfQuestions.push(questionOne)
-//const questionTwo = new Question(2,'Courier New','Arial','Comic Sans','Roboto',1)
-//deckOfQuestions.push(questionOne,questionTwo)
+const questionTwo = new Question(2,'Courier New','Arial','Comic Sans','Roboto',1)
+deckOfQuestions.push(questionOne,questionTwo)
+
 presentQuestion(deckOfQuestions[0])
 
 function presentQuestion(deckQuestion){
@@ -41,14 +48,14 @@ function presentQuestion(deckQuestion){
 function checkAnswer(question,userInput){
     if (question.answer == userInput.dataset.answerNum){
         userInput.style.backgroundColor = "lightgreen"
-        resultEl.innerText = "Correct! Onto the next question."
+        resultTextEl.innerText = "Correct!"
         updateScore()
     }
     else{
         const correctAnswer = document.querySelector(`[data-answer-num='${question.answer}']`)
         correctAnswer.style.backgroundColor = "lightgreen"
         userInput.style.backgroundColor = "lightcoral"
-        resultEl.innerText = "Sorry, that's incorrect. Onto the next question."
+        resultTextEl.innerHTML = "Sorry, that's incorrect."
     }
     answersEl.style.pointerEvents = 'none'
 }
@@ -75,38 +82,28 @@ function setStringFont(question){
 function setAnswers(question){
     questionNumEl.innerHTML = question.qNum
 
-    const ans1 = document.createElement('a')
     ans1.innerText = question.font1
-    ans1.dataset.answerNum = '1'
-    answersEl.appendChild(ans1)
-
-    const ans2 = document.createElement('a')
     ans2.innerText = question.font2
-    ans2.dataset.answerNum = '2'
-    answersEl.appendChild(ans2)
-
-    const ans3 = document.createElement('a')
     ans3.innerText = question.font3
-    ans3.dataset.answerNum = '3'
-    answersEl.appendChild(ans3)
-
-    const ans4 = document.createElement('a')
     ans4.innerText = question.font4
-    ans4.dataset.answerNum = '4'
-    answersEl.appendChild(ans4)
 }
 
 resetEl.addEventListener('click',function(evt){
     evt.preventDefault()
     score = 0
     scoreEl.innerText = score
-    resultEl.innerText = ""
+    resultTextEl.innerText = "Result"
     answersEl.style.pointerEvents = "auto"
     for (let i = 0; i < answersEl.childElementCount; i++){
         answersEl.children[i].style.backgroundColor = "white"
     }
 })
 
+
+nextQuestion.addEventListener('click', function(evt){
+    console.log("Next question!")
+    presentQuestion(deckOfQuestions[1])
+})
 /* 
 function sentenceGenerator(){
     //generate a line of Lorem Ipsum
